d254ea925ea068690ecf8b28db1f525899a4f231
Jismatov895 <Jismatov895@gmail.com>
2024-05-15 04:08:25 +0500

    Added Prescription DTO

---
+++ Hospital.Service/DTOs/Prescription/PrescriptionCreateModel.cs
@@ -0,0 +1,13 @@
+﻿using Hospital.Domain.Entities;
+
+namespace Hospital.Service.DTOs.Prescription
+{
+    public class PrescriptionCreateModel
+    {
+        public long UserId { get; set; }
+        public User User { get; set; }
+        public long DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ Hospital.Service/DTOs/Prescription/PrescriptionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Hospital.Domain.Entities;
+
+namespace Hospital.Service.DTOs.Prescription
+{
+    public class PrescriptionViewModel
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public User User { get; set; }
+        public long DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
