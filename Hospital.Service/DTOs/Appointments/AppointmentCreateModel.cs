dbd1943c84a97b75c3139e7fb8382baa79f025c0
Jismatov895 <Jismatov895@gmail.com>
2024-05-15 04:11:50 +0500

    Added Appointment DTO

---
+++ Hospital.Service/DTOs/Appointments/AppointmentCreateModel.cs
@@ -0,0 +1,13 @@
+﻿using Hospital.Domain.Entities;
+
+namespace Hospital.Service.DTOs.Appointments
+{
+    public class AppointmentCreateModel
+    {
+        public long UserId { get; set; }
+        public User User { get; set; }
+        public long DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
