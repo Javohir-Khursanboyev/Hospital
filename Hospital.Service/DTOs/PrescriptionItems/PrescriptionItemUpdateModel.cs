2bed4e63fd53e63374d1b50802957b652a3cf6d2
Jismatov895 <Jismatov895@gmail.com>
2024-05-15 04:04:03 +0500

    Added Prescription Item DTO

---
+++ Hospital.Service/DTOs/PrescriptionItems/PrescriptionItemUpdateModel.cs
@@ -0,0 +1,14 @@
+﻿using Hospital.Domain.Entities;
+
+namespace Hospital.Service.DTOs.PrescriptionItems
+{
+    public class PrescriptionItemUpdateModel
+    {
+        public long Id { get; set; }
+        public long PrescriptionId { get; set; }
+        public Prescription Prescription { get; set; }
+        public string MedicineName { get; set; }
+        public string MedicineUsage { get; }
+        public int Days { get; set; }
+    }
+}
