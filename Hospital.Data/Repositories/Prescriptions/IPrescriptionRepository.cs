d07405e8835ba65631865a09f3423a230a054ae9
axrordev <axror.algo@gmail.com>
2024-05-15 12:50:22 +0500

    added prescription repository

---
+++ Hospital.Data/Repositories/Prescriptions/IPrescriptionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Hospital.Domain.Entities;
+
+namespace Hospital.Data.Repositories.Prescriptions;
+
+public interface IPrescriptionRepository
+{
+    Task<Prescription> InsertAsync(Prescription prescription);
+    Task<Prescription> UpdateAsync(Prescription prescription);
+    Task<Prescription> DeleteAsync(Prescription prescription);
+    Task<Prescription> SelectAsync(long id);
+    Task<IEnumerable<Prescription>> SelectAllAsEnumerableAsync();
+    Task<IQueryable<Prescription>> SelectAllAsQuerableAsync();
+    Task<bool> SaveAsync();
+}
---
+++ Hospital.Data/Repositories/Prescriptions/PrescriptionRepository.cs
@@ -0,0 +1,54 @@
+﻿using Hospital.Data.DbContexts;
+using Hospital.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Data.Repositories.Prescriptions;
+
+public class PrescriptionRepository : IPrescriptionRepository
+{
+    private AppDbContext context;
+    public PrescriptionRepository(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<Prescription> InsertAsync(Prescription prescription)
+    {
+        return (await context.Prescriptions.AddAsync(prescription)).Entity;
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return (await context.SaveChangesAsync()) > 0;  //  we use > 0 🧘
+    }
+
+    public async Task<IEnumerable<Prescription>> SelectAllAsEnumerableAsync()
+    {
+        return await context.Prescriptions.Where(pr => !pr.IsDeleted).ToListAsync();
+    }
+
+    public async Task<IQueryable<Prescription>> SelectAllAsQuerableAsync()
+    {
+        return await Task.FromResult(context.Prescriptions.AsQueryable().Where(pr => !pr.IsDeleted));
+    }
+
+    public async Task<Prescription> SelectAsync(long id)
+    {
+#pragma warning disable CS8603 // Possible null reference return.
+        return await context.Prescriptions.FirstOrDefaultAsync(x => x.Id == id);
+#pragma warning restore CS8603 // Possible null reference return.
+    }
+
+    public async Task<Prescription> UpdateAsync(Prescription prescription)
+    {
+        context.Prescriptions.Update(prescription);
+        return await Task.FromResult(prescription);
+    }
+    public async Task<Prescription> DeleteAsync(Prescription prescription)
+    {
+        prescription.IsDeleted = true;
+        context.Prescriptions.Update(prescription);
+        return await Task.FromResult(prescription);
+    }
+
+}
