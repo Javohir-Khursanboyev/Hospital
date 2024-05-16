48e80618a59f726f24c8da376d31fa7e3ad520f8
Jismatov895 <Jismatov895@gmail.com>
2024-05-16 15:09:51 +0500

    Added User Contact DTO

---
+++ Hospital.Service/DTOs/UserContacts/UserContactCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.Service.DTOs.UserContacts
+{
+    public class UserContactCreateModel
+    {
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public long UserId { get; set; }
+    }
+}
