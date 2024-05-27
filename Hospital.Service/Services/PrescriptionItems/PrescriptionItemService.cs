b1e9f93b7d6406f5090f33777ec335c53c0d28d4
axrordev <axror.algo@gmail.com>
2024-05-28 00:25:16 +0500

    Update PrescriptionItem service

--- Hospital.Service/Services/PrescriptionItems/PrescriptionItemService.cs
+++ Hospital.Service/Services/PrescriptionItems/PrescriptionItemService.cs
@@ -12,7 +12,7 @@ public class PrescriptionItemService(IMapper mapper, IPrescriptionItemRepo presc
     public async Task<PrescriptionItemViewModel> CreateAsync(PrescriptionItemCreateModel model)
     {
         var existPrescriptionItem = (await prescriptionItemRepo.SelectAllAsQuerableAsync())
-            .FirstOrDefault(p => p.MedicineName == model.MedicineName && p.PrescriptionId == model.PrescriptionId && !p.IsDeleted)
+            .FirstOrDefault(p => p.MedicineName == model.MedicineName && p.PrescriptionId == model.PrescriptionId)
             ?? throw new AlreadyExistException($"Prescription Item is already exist with this Medicine name{model.MedicineName}");
 
         var mappedPrescriptionItem = mapper.Map<PrescriptionItem>(model);
@@ -20,6 +20,26 @@ public class PrescriptionItemService(IMapper mapper, IPrescriptionItemRepo presc
         await prescriptionItemRepo.SaveAsync();
 
         return mapper.Map<PrescriptionItemViewModel>(mappedPrescriptionItem);
+    }
+
+    public async Task<PrescriptionItemViewModel> UpdateAsync(long id, PrescriptionItemUpdateModel model)
+    {
+        var existPrescriptionItem = await prescriptionItemRepo.SelectAsync(id)
+            ?? throw new NotFoundException($"Appointment is not found with this id: {id}");
+
+        var alreadyExistPrescriptionItem = (await prescriptionItemRepo.SelectAllAsQuerableAsync())
+            .FirstOrDefault(p => p.Id != id && p.PrescriptionId == model.PrescriptionId);
+
+        if (alreadyExistPrescriptionItem is not null)
+            throw new AlreadyExistException($"Prescription Item is already exist with PrescriptionId {model.PrescriptionId}");
+
+        mapper.Map(model, existPrescriptionItem);
+        existPrescriptionItem.UpdatedAt = DateTime.UtcNow;
+
+        var updatedPrescriptionItem = await prescriptionItemRepo.UpdateAsync(existPrescriptionItem);
+        await prescriptionItemRepo.SaveAsync();
+
+        return mapper.Map<PrescriptionItemViewModel>(updatedPrescriptionItem);
     }
 
     public async Task<bool> DeleteAsync(long id)
@@ -50,24 +70,5 @@ public class PrescriptionItemService(IMapper mapper, IPrescriptionItemRepo presc
         return mapper.Map<PrescriptionItemViewModel>(existPrescriptionItem);
     }
 
-    public async Task<PrescriptionItemViewModel> UpdateAsync(long id, PrescriptionItemUpdateModel model)
-    {
-        var existPrescriptionItem = (await prescriptionItemRepo.SelectAllAsQuerableAsync()).FirstOrDefault(p => p.Id == id && !p.IsDeleted)
-         ?? throw new NotFoundException($"Prescription Item is not found with this id: {id}");
-        var notUpdatedPrescription = (await prescriptionItemRepo.SelectAllAsQuerableAsync())
-            .FirstOrDefault(p => p.Id != id && !p.IsDeleted && p.MedicineName == model.MedicineName && p.PrescriptionId == model.PrescriptionId);
-        if (notUpdatedPrescription is not null)
-            throw new AlreadyExistException($"This Prescription Item is not updated, because the Prescription Item is already exist with {model.MedicineName} Medicine name and {model.PrescriptionId} Prescription Id");
-
-        existPrescriptionItem.MedicineName = model.MedicineName;
-        existPrescriptionItem.PrescriptionId = model.PrescriptionId;
-        existPrescriptionItem.Days = model.Days;
-        mapper.Map(model, existPrescriptionItem);
-        existPrescriptionItem.UpdatedAt = DateTime.UtcNow;
-
-        var updatedPrescriptionItem = await prescriptionItemRepo.UpdateAsync(existPrescriptionItem);
-        await prescriptionItemRepo.SaveAsync();
-
-        return mapper.Map<PrescriptionItemViewModel>(updatedPrescriptionItem);
-    }
+    
 }
